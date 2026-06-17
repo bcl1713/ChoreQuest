626c53e2a8d46c6c2381e5d537f82bad6595bb62
bcl1713 <bcl1713@gmail.com>
2026-06-16 21:29:41 -0500

    refactor: colocate quest create modal tests (#193)
    Reviewer gate passed for bounded quest-create-modal test colocation slice. Public pass evidence posted on PR #193; GitHub Playwright smoke suite green.

--- components/quests/quest-create-modal/adhoc-quest-form.test.tsx
+++ components/quests/quest-create-modal/adhoc-quest-form.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import AdhocQuestForm from "../adhoc-quest-form";
+import AdhocQuestForm from "./adhoc-quest-form";
 import { QuestDifficulty, QuestCategory } from "@/lib/types/database";
 
 describe("AdhocQuestForm", () => {
